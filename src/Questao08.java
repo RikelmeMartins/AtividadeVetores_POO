506ed6fe4c133437ae57a1eb04fa9fa71ce472ca
RikelmeMartins <rikelme2005@gmail.com>
2025-09-01 21:14:54 -0300

    Atividade vetores

--- src/Questao08.java
+++ src/Questao08.java
@@ -3,15 +3,9 @@ public class Questao08 {
     public static void main(String[] args) {
 
         int[] numeros = new int[]{1, 2, 3, 4, 5};
-        int[] numerosInversos = new int[5];
-        int ind = 4;
-        for (int i = 0; i < numeros.length; i++){
-            numerosInversos[ind] = numeros[i];
-            ind--;
-        }
 
-        for (int numero : numerosInversos){
-            System.out.println(numero);
+        for (int i = numeros.length-1; i >= 0; i--){
+            System.out.println(numeros[i]);
         }
 
     }
